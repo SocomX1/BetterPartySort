6e2aa8ea4d9cbee4fd943371c6b9e3c4a11e2f99
Alex Zucker <SocomX01@gmail.com>
2024-11-18 18:34:48 -0800

    HELP

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -1,10 +1,21 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Text;
+using Dalamud.Game.Addon.Lifecycle;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using Dalamud.Game.Command;
+using Dalamud.Game.Config;
+using Dalamud.Interface.Internal.Windows.Data.Widgets;
 using Dalamud.Interface.Windowing;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
+using Dalamud.Utility;
 using FFXIVClientStructs.FFXIV.Client.Game.Group;
+using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 using SamplePlugin.Windows;
 
 namespace SamplePlugin;
@@ -23,7 +34,17 @@ public sealed class Plugin : IDalamudPlugin
     [PluginService]
     internal static IPartyList PartyList { get; private set; } = null!;
 
+    [PluginService]
+    internal static IGameConfig GameConfig { get; private set; } = null!;
+
+    [PluginService]
+    internal static IPluginLog PluginLog { get; private set; } = null!;
+
+    [PluginService]
+    internal static IAddonLifecycle AddonLifecycle { get; private set; } = null!;
+
     private const string CommandName = "/pmycommand";
+    private const string DebugCommandName = "/sortdata";
 
     public Configuration Configuration { get; init; }
 
@@ -49,6 +70,11 @@ public sealed class Plugin : IDalamudPlugin
             HelpMessage = "A useful message to display in /xlhelp"
         });
 
+        CommandManager.AddHandler(DebugCommandName, new CommandInfo(OnDebugCommand)
+        {
+            HelpMessage = "Output party and sort order values to console"
+        });
+
         PluginInterface.UiBuilder.Draw += DrawUI;
 
         // This adds a button to the plugin installer entry of this plugin which allows
@@ -57,13 +83,75 @@ public sealed class Plugin : IDalamudPlugin
 
         // Adds another button that is doing the same but for the main ui of the plugin
         PluginInterface.UiBuilder.OpenMainUi += ToggleMainUI;
+
+        AddonLifecycle.RegisterListener(AddonEvent.PreRequestedUpdate, "_PartyList", OnPreRequestUpdate);
     }
 
-    private unsafe void sortParty()
+    private unsafe void debug()
     {
-        var partyManager = GroupManager.Instance();
-        var partyGroup = partyManager->GetGroup();
-        var party = partyGroup->PartyMembers;
+        // foreach (var partyMember in AgentHUD.Instance()->PartyMembers)
+        // {
+        //     PluginLog.Info(partyMember.Index + ":");
+        // }
+        //
+        // var char1 = AgentHUD.Instance()->PartyMembers[0];
+        // var char2 = AgentHUD.Instance()->PartyMembers[1];
+        //
+        // AgentHUD.Instance()->PartyMembers[0] = char2;
+        // AgentHUD.Instance()->PartyMembers[1] = char1;
+
+
+        // var partyManager = GroupManager.Instance()->MainGroup;
+
+        // PluginLog.Info(PartyList.Length + " party members");
+        // PluginLog.Info(PartyList.PartyId.ToString());
+
+        // PluginLog.Info(partyManager.PartyId + " id");
+
+        // foreach (var partyMember in partyManager.PartyMembers)
+        // {
+        //     PluginLog.Info(partyMember.NameString);
+        // }
+
+        // PluginLog.Info(partyManager.PartyMembers[0].NameString);
+        // PluginLog.Info(partyManager.PartyMembers[1].NameString);
+
+        // var char1 = partyManager.PartyMembers[0];
+        // var char2 = partyManager.PartyMembers[1];
+        // partyManager.PartyMembers[0] = char2;
+        // partyManager.PartyMembers[1] = char1;
+
+
+        // var uiConfigSection = GameConfig.UiConfig;
+        // var dpsOrder =
+        //     uiConfigSection.GetUInt(UiConfigOption.PartyListSortTypeDps.GetAttribute<GameConfigOptionAttribute>().Name);
+        // GameConfig.TryGet(UiConfigOption.PartyListSortTypeDps, out UIntConfigProperties? configProperties);
+
+        // GameConfig.Changed += LogUIChange;
+
+        // PluginLog.Info(configProperties.ToString());
+
+        // var partyList = Framework.Instance()->GetUIModule()->GetRaptureUiDataModule()->PartyListHealerOrder;
+        // foreach (var job in partyList)
+        // {
+        //     PluginLog.Info(job.ToString());   
+        // }
+
+        // var partyList =
+        //     (AddonPartyList*)Framework.Instance()->GetUIModule()->GetRaptureAtkModule()->AtkUnitManager->GetAddonByName(
+        //         "_PartyList");
+        // var party = partyList->TrustMembers;
+        // var char1 = party[5];
+        // var char2 = party[6];
+        // party[6] = char1;
+        // party[5] = char2;
+        // PluginLog.Info(partyList->TrustCount.ToString());
+    }
+
+    private void OnPreRequestUpdate(AddonEvent eventType, AddonArgs args)
+    {
+        var numericArgs = ((AddonRequestedUpdateArgs)args).NumberArrayData;
+        var stringArgs = ((AddonRequestedUpdateArgs)args).StringArrayData;
     }
 
     public void Dispose()
@@ -81,6 +169,30 @@ public sealed class Plugin : IDalamudPlugin
         // in response to the slash command, just toggle the display status of our main ui
         ToggleMainUI();
     }
+
+    private void OnDebugCommand(string command, string args)
+    {
+        string[] strings = args.Split(' ');
+        swapPartyMembers(Convert.ToInt32(strings[0]), Convert.ToInt32(strings[1]));
+    }
+
+    private unsafe void swapPartyMembers(int index1, int index2)
+    {
+        var partyList =
+            (AddonPartyList*)Framework.Instance()->GetUIModule()->GetRaptureAtkModule()->AtkUnitManager->GetAddonByName(
+                "_PartyList");
+        var party = partyList->TrustMembers;
+        var char1 = party[index1];
+        var char2 = party[index2];
+        party[index1] = char2;
+        party[index2] = char1;
+    }
+
+    // public void LogUIChange(object? sender, ConfigChangeEvent e)
+    // {
+    //     PluginLog.Info(sender.ToString());
+    //     PluginLog.Info(e.ToString());
+    // }
 
     private void DrawUI() => WindowSystem.Draw();
 
